be98c5ff8c14358f8c14626d2248b509fd91d3ed
Han <apricot13@users.noreply.github.com>
2024-09-10 16:16:15 +0100

    metadata

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,19 +12,39 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#0b0c0c",
     icons: [
       {
+        src: "/govuk-assets/images/favicon.ico",
+        type: "image/x-icon",
+        sizes: "48x48",
+      },
+      {
+        src: "/govuk-assets/images/favicon.svg",
+        type: "image/svg+xml",
+        sizes: "150x150",
+        purpose: "any",
+      },
+      {
         src: "/govuk-assets/images/govuk-icon-180.png",
-        sizes: "180x180",
         type: "image/png",
+        sizes: "180x180",
+        purpose: "maskable",
       },
       {
         src: "/govuk-assets/images/govuk-icon-192.png",
-        sizes: "192x192",
         type: "image/png",
+        sizes: "192x192",
+        purpose: "maskable",
       },
       {
         src: "/govuk-assets/images/govuk-icon-512.png",
-        sizes: "512x512",
         type: "image/png",
+        sizes: "512x512",
+        purpose: "maskable",
+      },
+      {
+        src: "/govuk-assets/images/govuk-icon-mask.svg",
+        type: "image/svg+xml",
+        sizes: "150x150",
+        purpose: "monochrome",
       },
     ],
   };
